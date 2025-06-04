450085b49c9155ac34f9e01bfa0b287c40b5ee88
miskogepa <milovan.jankovic50m@gmail.com>
2025-06-04 20:07:56 +0200

    Implement password generator UI with input and range slider

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -11,7 +11,40 @@ function App() {
   const [password, setPassword] = useState(""); // generisana lozinka
 
   return (
-   <div className=""></div>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 bg-gray-800 text-orange-500">
+      <h1 className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 bg-gray-800 text-orange-500">
+        Password generator
+      </h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full px-4 py-2"
+          placeholder="password"
+          readOnly
+        />
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          {" "}
+          copy
+        </button>
+      </div>
+
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1 ">
+          <input
+            type="range"
+            min={8}
+            max={20}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
+            name=""
+            id=""
+          />
+          <label htmlFor="lenght">Lenght {length}</label>
+        </div>
+      </div>
+    </div>
   );
 }
 
